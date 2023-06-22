bcb6d5913d764bbddc849d5c99fa0060e8a4879c
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-23 04:47:43 +0700

    improvement: change middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { login, logout, refreshToken, register } from "../controllers/auth.js";
+import verifyRefreshToken from "../middleware/refreshToken.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/refreshToken", refreshToken);
+router.post("/refreshToken", verifyRefreshToken, refreshToken);
 router.post("/logout", logout);
 
 export default router;
